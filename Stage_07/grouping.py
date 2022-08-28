1bf388a716b7f39f826208aab3fedb4f952a5776
Theo <ckciun@gmail.com>
2022-08-28 20:22:56 +0900

    Twelfth Commit

--- Stage_07/grouping.py
+++ Stage_07/grouping.py
@@ -1,6 +1,10 @@
 ## grouping ()
 import re
-p = re.compile('(ABC)+')
-m = p.search('ABCABCABC')
-print(m)
-print(m.group())
+# p = re.compile('(ABC)+')
+# m = p.search('ABCABCABC')
+# print(m)
+# print(m.group())
+
+# p = re.compile(r'(\w+)\s+\d+[-]\d+[-]\d+')
+# m = p.search("park 010-1234-5678")
+# print(m.group(1))
