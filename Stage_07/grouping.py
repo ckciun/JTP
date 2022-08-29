ff5601c8ffa6d57d70108e64ac500afcfe136346
Theo <ckciun@gmail.com>
2022-08-30 00:27:07 +0900

    Thirheenth Commit

--- Stage_07/grouping.py
+++ Stage_07/grouping.py
@@ -20,16 +20,3 @@ import re
 # p = re.compile(r"(?P<name>\w+)\s+((\d+)[-]\d+[-]\d+)")
 # m = p.search("park 010-1234-5678")
 # print(m.group("name"))
-
-## 전방탐색: 긍정형 (?=)
-
-# p = re.compile(".+:")
-# m = p.search("http://google.com")
-# print(m.group())
-
-## 검색 조건에는 포함되지만 결과에선 제외시킨다.
-## (?=[제외시킬 값])
-
-p = re.compile(".+(?=:)")
-m = p.search("http://google.com")
-print(m.group())
--- Stage_07/replace.py
+++ Stage_07/replace.py
@@ -3,9 +3,3 @@
 import re
 # p = re.compile('(blue|white|red)')
 # print(p.sub('colour', 'blue socks and red shoes'))
-
-## Greedy vs Non-Greedy
-
-s = '<html><head><title>Title</title>'
-print(re.match('<.*>', s).group())  #Greedy
-print(re.match('<.*?>', s).group()) #Non-Greedy
